f1de7b3fefb8c8958759ac57c0235741a5f78bf9
liuching <benjack30518@gmail.com>
2020-10-13 15:51:43 +0800

    1013 add clock

---
+++ Clock/Clock.srcs/sources_1/new/clock.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/13/2020 02:46:04 PM
+// Design Name: 
+// Module Name: clock
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module clock(clock, digit, dot, );
+endmodule
